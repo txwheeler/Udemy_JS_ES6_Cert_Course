3f50fdcb63a634bf2774f89539062d3ce3dcad7f
txwheeler <txwheeler@gmail.com>
2021-04-09 00:25:13 -0500

    first 3 sections
    first 3 sections

--- Readme.md
+++ Readme.md
@@ -1,1 +1,4 @@
 Udemy Course - JavaScript 2019: JavaScript ES6 Certification Course by Mark Lassoff
+
+Note:
+- VSC w/ live server extension
---
+++ Section 1/Video_Follow_Along/first.js
@@ -0,0 +1,4 @@
+alert('Hello World! Welcome to JavaScript!');
+alert('This is the second alert');
+alert('Hello World! My name is Jonathan, I am a student.')
+
