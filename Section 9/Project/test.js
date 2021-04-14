972fe4855940807992dc27170f2c3537a89fbc2a
txwheeler <txwheeler@gmail.com>
2021-04-14 16:40:53 -0500

    test
    test

---
+++ Section 9/Project/test.js
@@ -0,0 +1,17 @@
+function helloworld(x){
+	alert(x);
+}
+
+
+function multiply(x,y,z,a,b,c){
+	return x * y * z * a * b * c;
+}
+
+let a = (msg) => ('here ' + a);
+
+
+b = a('tetstsdfalkfskl')
+
+
+
+
