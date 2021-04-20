18f6faa2ab73947bbbac58cf4e5c0a352a5fff59
txwheeler <txwheeler@gmail.com>
2021-04-19 23:15:50 -0500

    section 16 project 1
    project 1 in section 16 - tip calculator

---
+++ Section 16/Tip_Calculator/tip.js
@@ -0,0 +1,20 @@
+const range = document.getElementById('tipRange');
+const tipOut = document.getElementById('percentOut');
+const btnCalculate = document.getElementById('btnCalculate');
+const billAmount = document.getElementById('billAmount');
+const resultArea = document.getElementById('result');
+
+range.ondrag = function(){
+	tipOut.innerHTML = range.value + '%';
+}
+
+btnCalculate.onclick = function(){
+	let theBill = parseFloat(billAmount.value);
+	let tip = (theBill * (range.value/100)).toFixed(2);
+	let total = (theBill + parseFloat(tip)).toFixed(2);
+	let out = `<strong>Tip Amount:</strong>$${tip}<br/><strong>Total Bill:</strong>$${total}`;
+	resultArea.innerHTML = out;
+}
+
+
+
